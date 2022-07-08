0eb25831fe9a2eb29139c53e3c61b24d889b6b3b
Cami <camilacassimiro@gmail.com>
2022-07-08 10:28:25 -0300

    ex 01 enviado

---
+++ modulo2-semana6-api/Controllers/ExercicioVerdadeiroFalsoController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace modulo2_semana6_api.Controllers;
+
+///<summary>
+///Exercicio 1
+///</summary>
+[ApiController]
+[Route("[controller]")]
+public class ExercicioVerdadeiroFalsoController : ControllerBase
+{
+  ///<summary>
+  /// Implementar a regra do exercicio 1 aqui dentro do método GET
+  /// </summary>
+  /// <param name="tipo"></param>
+  /// <returns></returns>
+  [HttpGet("{tipo}")]
+  public string Get(string tipo)
+  {
+    if (tipo != "Verdadeiro" && tipo != "Falso")
+    {
+      return "Texto diferente de verdadeiro ou falso";
+    }
+    return tipo;
+  }
+}
